aea2685bd56024000e0f3e4d96f3a4efb0a1a78d
JimGerth <jim.frederic.gerth@gmail.com>
2020-12-05 00:31:20 +0100

    main: Add command line argument check
    Add simple command line argument check to be able to specify what day's
    puzzle to run.
    Right now this expects a sinle number as the first argument, which could
    definitely be improved, to maybe check for a flag, like "-days=3" or
    "-d 3".
    
    Signed-off-by: JimGerth <jim.frederic.gerth@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let day: i32 = args
+        .get(1)
+        .expect("Specify what day's puzzle to run by typing in a number.")
+        .parse()
+        .expect("Only use a single number to specify what day's puzzle to run.");
+
+    match day {
+        _ => println!("Sorry, this puzzle is not implemented yet."),
+    }
 }
