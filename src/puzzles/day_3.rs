d6fbb6600d677eb8e8440cd0b1de086fb74076f4
JimGerth <jim.frederic.gerth@gmail.com>
2020-12-05 22:48:17 +0100

    puzzles/day_3: Implement both parts of the puzzle
    Signed-off-by: JimGerth <jim.frederic.gerth@gmail.com>

--- src/puzzles/day_3.rs
+++ src/puzzles/day_3.rs
@@ -1,7 +1,77 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
 pub fn part_1() {
-    println!("Sorry, this puzzle is not implemented yet.");
+    let file = File::open("src/inputs/day_3.txt").unwrap();
+    let mut lines = io::BufReader::new(file).lines().enumerate();
+
+    let mut trees = 0;
+
+    while let Some((i, Ok(line))) = lines.next() {
+        if line.chars().nth(i * 3 % line.len()).unwrap() == '#' {
+            trees += 1;
+        }
+    }
+
+    println!("The answer to part 1 is {}", trees);
+}
+
+struct Slope {
+    right: usize,
+    down: usize,
+    trees: usize,
 }
 
 pub fn part_2() {
-    println!("Sorry, this puzzle is not implemented yet.");
+    let file = File::open("src/inputs/day_3.txt").unwrap();
+    let mut lines = io::BufReader::new(file).lines().enumerate();
+
+    let mut slopes = vec![
+        Slope {
+            right: 1,
+            down: 1,
+            trees: 0,
+        },
+        Slope {
+            right: 3,
+            down: 1,
+            trees: 0,
+        },
+        Slope {
+            right: 5,
+            down: 1,
+            trees: 0,
+        },
+        Slope {
+            right: 7,
+            down: 1,
+            trees: 0,
+        },
+        Slope {
+            right: 1,
+            down: 2,
+            trees: 0,
+        },
+    ];
+
+    while let Some((i, Ok(line))) = lines.next() {
+        for slope in slopes.iter_mut() {
+            if i % slope.down == 0
+                && line
+                    .chars()
+                    .nth(i * slope.right / slope.down % line.len())
+                    .unwrap()
+                    == '#'
+            {
+                slope.trees += 1;
+            }
+        }
+    }
+
+    println!(
+        "The answer to part 2 is {}",
+        slopes
+            .iter()
+            .fold(1, |product, slope| product * slope.trees)
+    );
 }
