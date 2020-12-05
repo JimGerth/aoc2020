22408b3da74aaa83942f0a82eb1573a48c34efb3
JimGerth <jim.frederic.gerth@gmail.com>
2020-12-05 22:30:13 +0100

    puzzles/day_2: Implemented second part of day 2 puzzle
    Signed-off-by: JimGerth <jim.frederic.gerth@gmail.com>

--- src/puzzles/day_2.rs
+++ src/puzzles/day_2.rs
@@ -51,5 +51,35 @@ pub fn part_1() {
 }
 
 pub fn part_2() {
-    println!("Sorry, this puzzle is not implemented yet.");
+    let file = File::open("src/inputs/day_2.txt").unwrap();
+    let mut lines = io::BufReader::new(file).lines();
+
+    // Format of the input lines with capture groups ("(...)")
+    // to extract the passowrd rule and passowrd.
+    let regex = Regex::new(r"^(\d+)-(\d+) (.): (.*)$").unwrap();
+
+    let mut matches = 0;
+
+    while let Some(Ok(line)) = lines.next() {
+        print!("{}", line);
+
+        // Extract password rule and password from line.
+        let caps = regex.captures(line.as_str()).unwrap();
+        let letter = caps.get(3).unwrap().as_str().chars().next().unwrap();
+        let first: usize = caps.get(1).unwrap().as_str().parse().unwrap();
+        let second: usize = caps.get(2).unwrap().as_str().parse().unwrap();
+        let password: Vec<char> = caps.get(4).unwrap().as_str().chars().collect();
+
+        // Check if the passowrd matches the rule.
+        if (password[first - 1] == letter && password[second - 1] != letter)
+            || (password[first - 1] != letter && password[second - 1] == letter)
+        {
+            println!(" is a match!");
+            matches += 1;
+        } else {
+            println!(" is not a match...");
+        }
+    }
+
+    println!("The answer to part 2 is: There are {} matches.", matches);
 }
