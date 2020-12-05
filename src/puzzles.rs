efe59a0f0f5f575445905c6acf5916c9e40a128f
JimGerth <jim.frederic.gerth@gmail.com>
2020-12-05 13:45:14 +0100

    puzzles: Use new day 2 module to run second puzzle
    Signed-off-by: JimGerth <jim.frederic.gerth@gmail.com>

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -1,4 +1,5 @@
 mod day_1;
+mod day_2;
 
 pub fn run(day: i32) {
     match day {
@@ -6,6 +7,11 @@ pub fn run(day: i32) {
             println!("Running puzzle from day 1...");
             day_1::part_1();
             day_1::part_2();
+        }
+        2 => {
+            println!("Running puzzle from day 2...");
+            day_2::part_1();
+            day_2::part_2();
         }
         _ => println!("Sorry, this puzzle is not implemented yet."),
     }
