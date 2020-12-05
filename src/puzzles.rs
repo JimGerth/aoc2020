102a84d96d52c4895fc23669fe77f2d8ac2ca2a0
JimGerth <jim.frederic.gerth@gmail.com>
2020-12-05 12:52:53 +0100

    puzzles: Use new module for day 1
    Signed-off-by: JimGerth <jim.frederic.gerth@gmail.com>

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -1,5 +1,11 @@
+mod day_1;
+
 pub fn run(day: i32) {
     match day {
+        1 => {
+            day_1::part_1();
+            day_1::part_2();
+        }
         _ => println!("Sorry, this puzzle is not implemented yet."),
     }
 }
