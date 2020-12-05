1020320b456200fca2882387aca8c7124f68c1ea
JimGerth <jim.frederic.gerth@gmail.com>
2020-12-05 13:31:49 +0100

    puzzles: Add println!()
    Signed-off-by: JimGerth <jim.frederic.gerth@gmail.com>

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -3,6 +3,7 @@ mod day_1;
 pub fn run(day: i32) {
     match day {
         1 => {
+            println!("Running puzzle from day 1...");
             day_1::part_1();
             day_1::part_2();
         }
