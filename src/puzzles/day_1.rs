dd12cdd09750a0fc44a38396275eb2dd581c5cd0
JimGerth <jim.frederic.gerth@gmail.com>
2020-12-05 13:25:45 +0100

    puzzles/day_1: Add read_input() to parse i32 array
    Signed-off-by: JimGerth <jim.frederic.gerth@gmail.com>

--- src/puzzles/day_1.rs
+++ src/puzzles/day_1.rs
@@ -1,7 +1,24 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
 pub fn part_1() {
     println!("Sorry, this puzzle is not implemented yet.");
 }
 
 pub fn part_2() {
     println!("Sorry, this puzzle is not implemented yet.");
+}
+
+fn read_input() -> Vec<i32> {
+    let file = File::open("src/inputs/day_1.txt")
+        .expect("Missing input file! Expected at src/inputs/day_1.txt");
+    let lines: Vec<i32> = io::BufReader::new(file)
+        .lines()
+        .map(|line| {
+            line.unwrap()
+                .parse()
+                .expect("Non numeric value in input file!")
+        })
+        .collect();
+    return lines;
 }
