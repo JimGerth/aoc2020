b7222a6848ab73e0b67862b9775c7ff062285d1f
JimGerth <jim.frederic.gerth@gmail.com>
2020-12-05 22:39:46 +0100

    puzzles: Use new day 3 module to run puzzle
    Signed-off-by: JimGerth <jim.frederic.gerth@gmail.com>

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -1,5 +1,6 @@
 mod day_1;
 mod day_2;
+mod day_3;
 
 pub fn run(day: i32) {
     match day {
@@ -12,6 +13,11 @@ pub fn run(day: i32) {
             println!("Running puzzle from day 2...");
             day_2::part_1();
             day_2::part_2();
+        }
+        3 => {
+            println!("Running puzzle from day 3...");
+            day_3::part_1();
+            day_3::part_2();
         }
         _ => println!("Sorry, this puzzle is not implemented yet."),
     }
