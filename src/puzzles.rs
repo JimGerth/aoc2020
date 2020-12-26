aee342f08696bd32791ee2b6ae96bbfabf79c76c
Jim Gerth <jim.frederic.gerth@gmail.com>
2020-12-26 20:14:20 +0100

    puzzles: Use the moved tests module
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -3,6 +3,8 @@ mod day_2;
 mod day_3;
 mod day_4;
 
+mod tests;
+
 pub fn run(day: i32) {
     match day {
         1 => {
@@ -26,31 +28,5 @@ pub fn run(day: i32) {
             println!("The answer to part 2 is {}.", day_4::part_2());
         }
         _ => println!("Sorry, this puzzle is not implemented yet."),
-    }
-}
-
-mod tests {
-    #[test]
-    fn test_day_1() {
-        assert_eq!(super::day_1::part_1(), 32064);
-        assert_eq!(super::day_1::part_2(), 193598720);
-    }
-
-    #[test]
-    fn test_day_2() {
-        assert_eq!(super::day_2::part_1(), 640);
-        assert_eq!(super::day_2::part_2(), 472);
-    }
-
-    #[test]
-    fn test_day_3() {
-        assert_eq!(super::day_3::part_1(), 187);
-        assert_eq!(super::day_3::part_2(), 4723283400);
-    }
-
-    #[test]
-    fn test_day_4() {
-        assert_eq!(super::day_4::part_1(), 213);
-        assert_eq!(super::day_4::part_2(), 147);
     }
 }
