eee1c204616adc3e977bd55b7dfa4a16849b00ba
Jim Gerth <jim.frederic.gerth@gmail.com>
2020-12-26 20:07:16 +0100

    puzzles: Add test module and first tests
    The tests simply check that the puzzles functions return the correct answer.
    
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -28,3 +28,29 @@ pub fn run(day: i32) {
         _ => println!("Sorry, this puzzle is not implemented yet."),
     }
 }
+
+mod tests {
+    #[test]
+    fn test_day_1() {
+        assert_eq!(super::day_1::part_1(), 32064);
+        assert_eq!(super::day_1::part_2(), 193598720);
+    }
+
+    #[test]
+    fn test_day_2() {
+        assert_eq!(super::day_2::part_1(), 640);
+        assert_eq!(super::day_2::part_2(), 472);
+    }
+
+    #[test]
+    fn test_day_3() {
+        assert_eq!(super::day_3::part_1(), 187);
+        assert_eq!(super::day_3::part_2(), 4723283400);
+    }
+
+    #[test]
+    fn test_day_4() {
+        assert_eq!(super::day_4::part_1(), 213);
+        assert_eq!(super::day_4::part_2(), 147);
+    }
+}
