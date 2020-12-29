e4877ecda2020a6a50066ec562e624902f8f9d9a
Jim Gerth <jim.frederic.gerth@gmail.com>
2020-12-29 01:37:14 +0100

    puzzles/tests: Rename tests to not include 'test'
    Before the tests were named test_day_x.
    Since they are contained in the tests module they get referenced as tests::test_day_x.
    To remove the redundancy remove the 'test' from the tests names,
    so they get referenced as tests::day_x.
    
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- src/puzzles/tests.rs
+++ src/puzzles/tests.rs
@@ -1,29 +1,29 @@
 #[test]
-fn test_day_1() {
+fn day_1() {
     assert_eq!(super::day_1::part_1(), 32064);
     assert_eq!(super::day_1::part_2(), 193598720);
 }
 
 #[test]
-fn test_day_2() {
+fn day_2() {
     assert_eq!(super::day_2::part_1(), 640);
     assert_eq!(super::day_2::part_2(), 472);
 }
 
 #[test]
-fn test_day_3() {
+fn day_3() {
     assert_eq!(super::day_3::part_1(), 187);
     assert_eq!(super::day_3::part_2(), 4723283400);
 }
 
 #[test]
-fn test_day_4() {
+fn day_4() {
     assert_eq!(super::day_4::part_1(), 213);
     assert_eq!(super::day_4::part_2(), 147);
 }
 
 #[test]
-fn test_day_5() {
+fn day_5() {
     assert_eq!(super::day_5::part_1(), 838);
     assert_eq!(super::day_5::part_2(), Some(714));
 }
